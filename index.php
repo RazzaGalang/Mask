07a726a92356715089cfe99f289f0082557eb312
RazzaGalang <razzagalang45@gmail.com>
2021-12-27 14:39:51 +0700

    commit pertama

---
+++ index.php
@@ -0,0 +1,96 @@
+<html>
+    <head>
+        <title>
+            Home | mask.co
+        </title>
+
+        <link rel="stylesheet" type="text/css" href="style.css">
+
+    </head>
+
+    <body bgcolor="#900C3F">
+        <div id="box1">
+            <div id="box2">  
+                <font size="50">
+                    <i><b>
+                        <p align="center">  
+                           mask.co
+                        </p>
+                    </b></i>
+                </font>
+            </div>
+
+            <div id="box3">
+                <p align="center">
+                    <i>
+                        <b>
+                            <a href="mask.buff.php">Buff  |</a>
+                            <a href="mask.kain.php">  Kain  |</a>
+                            <a href="mask.medis.php">  Medis  |</a>
+                            <a href="mask.elektrik.php">  Elektrik  |</a>
+                            <a href="mask.r-series.php">  R Series  |</a>
+                            <a href="mask.p-series.php">  P Series  |</a>
+                            <a href="mask.n-series.php">  N Series  </a>
+                        </b>
+                    </i>
+                </p>
+            </div>
+
+            <div id="box4">
+                <div id="box5">
+                    <p align="center">
+                        <a href="index.html">Home</a>
+                    </p>
+                </div>
+
+                <div id="box6">
+                    <p align="left">
+                        <blockquote>
+                            <h2>
+                                Saat ini dianjurkan untuk memakai masker Covid 19 dobel untuk mencegah ganasnya virus Delta
+                            <hr size="1" color="black">
+                            </h2> <br>
+
+                            <h3>
+                                Penggunaan masker Covid 19 saat ini dianjurkan agar didobel supaya lebih aman. 
+                                Ini lantaran pandemi Covid 19 di Indonesia makin menjadi-jadi. 
+                                Jumlah kasus positif malah menunjukkan grafik yang meningkat. 
+                                Virus Corona yang menjadi biang keroknya seakan semakin menunjukkan keganasannya. <br><br>
+
+                                Salah satunya virus varian Delta yang mendadak terkenal karena menjadi sumber penyebab meningginya jumlah yang tertular Covid 19. 
+                                Disebutkan virus lebih mudah menularkan hingga 10x dibandingkan varian pertama Covid 19. <br><br>
+
+                                Hingga akhirnya muncul anjuran dari Satgas Covid 19 untuk menggunakan masker tidak cukup hanya 1, tapi 2 lapis. 
+                                Dianjurkan untuk memakai masker medis yang dilapisi dengan masker kain. 
+                                Tujuannya untuk lebih mampu menyaring kuman, virus atau partikel berbahaya lainnya. 
+                                Atau bisa juga cukup menggunakan 1 lapis masker namun harus jenis masker N95. <br>
+                            </h3>
+                        </blockquote>
+                    </p>
+                </div>
+
+                <div id="box7">
+                    <p align="right">
+                        <blockquote>
+                            Tentang penulis: <br>
+                            Razza Galang  <br>
+                            201114911 <br>
+                            XI-RPL A <br>
+                        </blockquote>
+                    </p>
+                </div>
+            </div>
+
+            <div id="box8">
+                <p align="center">
+                    <i>
+                        <b>
+                            Narahubung: <br>
+                            Instagram : @razzaglng   |  Line : @razzaglng  |  Telegram : @razzaglng
+                        </b>
+                    </i>
+                </p>
+            </div>
+        </div>
+    </body>
+</html>
